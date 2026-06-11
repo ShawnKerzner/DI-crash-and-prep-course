a7e1cb64cf5ff9dcd0934a321625c5a9b8b8694b
Shawn Kerzner <shawnkerz98@gmail.com>
2026-06-11 17:56:38 +0300

    adding intro exercise

---
+++ DI_Bootcamp_Stage1/py-intor-exercise.py
@@ -0,0 +1,35 @@
+first = "Hello World!"
+# This is a comment
+print("I AM A COMPUTER")
+if 1 < 2 and 4 > 2:
+    print("Math is fun")
+nope = None
+# 2 == 2 = True and False
+len_question = "What's my length"
+print(len(len_question))
+loud = "i am shouting"
+print(loud.upper())
+num_string = "1000"
+print(int(num_string))
+four = 4
+print(f"{4}real")
+print(3 * "cool")
+# print(1 / 0)
+empty_list = []
+user_name = input("What is your name?")
+print(f"Hello {user_name}")
+number = int(input("Pick a number"))
+if number < 0:
+    print("That number is less than 0")
+elif number > 0:
+    print("That number is greater than zero")
+else:
+    print("You picked 0")
+apple = "apple"
+index = apple.find('l')
+print(index)
+
+xylophone = "xylophone"
+if "y" in xylophone:
+    print("found!")
+print("my_string".islower())
