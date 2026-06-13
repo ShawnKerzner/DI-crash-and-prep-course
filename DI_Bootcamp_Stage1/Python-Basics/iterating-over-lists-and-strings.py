ae288cf07bc00db27551068cd4cce3d98df56bc5
Shawn Kerzner <shawnkerz98@gmail.com>
2026-06-13 11:33:22 +0300

    adding new file to folder

---
+++ DI_Bootcamp_Stage1/Python-Basics/iterating-over-lists-and-strings.py
@@ -0,0 +1,32 @@
+# Exercise 1
+# fruits = ["apple", "banana", "cherry", "date"]
+# for fruit in fruits:
+#     print(fruit)
+
+# for idx, char in enumerate("awesome"):
+#     print(idx, char)
+
+# i = 0
+# while i < 5:
+#     print(i)
+#     i +=1
+
+# count = 1
+# while count <= 5:
+#     print(count)
+#     count +=1
+
+# for num in [1, 2, 3, 4, 5, 6, 7]:
+#     if num % 2 == 0:
+#         continue
+#     elif num > 5:
+#         break
+#     print(num)
+
+# for num in range(4,10):
+#     print(num)
+
+capital_letters = []
+for num in range(65,91):
+    capital_letters.append(chr(num))
+print(capital_letters)
